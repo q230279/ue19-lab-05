6adf255489ca1616c8ff97ac7de1ec06705f1915
q230279 <julien.cinna@hotmail.fr>
2024-11-21 12:08:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import requests
 
-# URL de l'API avec paramètre de langue pour obtenir des blagues en français
+
 url = "https://v2.jokeapi.dev/joke/Any?lang=fr"
 
 response = requests.get(url)
 data = response.json()
 
-# Afficher la blague
+
 if data['type'] == 'single':
     print(data['joke'])
 else:
